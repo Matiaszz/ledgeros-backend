1961ae7b534a1f7494ed6adafc3cc010f86ce369
Allan Matias <alangiovanepaes@gmail.com>
2026-07-31 19:07:25 -0300

    fix: cors

--- src/main/java/com/ledgeros/shared/config/contracts/ResponseEntity.java
+++ src/main/java/com/ledgeros/shared/config/contracts/ResponseEntity.java
@@ -9,7 +9,7 @@ public interface ResponseEntity {
      default void setupHeaders(APIGatewayProxyResponseEvent responseEvent) {
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
-        headers.put("Access-Control-Allow-Origin", "http://localhost:5173");
+        headers.put("Access-Control-Allow-Origin", "https://ledgeros-react.vercel.app");
         headers.put("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
         headers.put("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
         responseEvent.setHeaders(headers);
